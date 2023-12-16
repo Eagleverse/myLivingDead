4f01169130b05b64d41e8ef7e43e2847310a4719
James Vo <48601146+Eagleverse@users.noreply.github.com>
2023-12-16 14:57:43 -0600

    Zombie accuracy
    Tradeoff attack power for accuracy. Tank's arms are heavy!

--- myLivingDead/src/main/java/Entity/Zombie/Type/CommonInfected.java
+++ myLivingDead/src/main/java/Entity/Zombie/Type/CommonInfected.java
@@ -4,6 +4,7 @@ import Entity.Entity;
 import Entity.Zombie.Zombie;
 
 public class CommonInfected extends Zombie {
+
     private int health = 30;
     final private int damage = 5;
     private boolean isAlive = true;
@@ -13,9 +14,22 @@ public class CommonInfected extends Zombie {
         typeCount = num;
     }
 
+    private int hit() {
+        // generate random number to determine if the zombie hits.
+        int max = 100;
+        int min = 1;
+        int randInt = min + (int) (Math.random() * ((max - min) + 1));
+        //These guys are accurate but not too strong.
+        if (randInt <= 95) { //Their arms meet their target 95% of the time.
+            return damage;
+        } else { // Missed.
+            return 0;
+        }
+    }
+
     @Override
     public void doAttack(Entity target) {
-        target.onAttack(damage);
+        target.onAttack(hit());
     }
 
     @Override
--- myLivingDead/src/main/java/Entity/Zombie/Type/Tank.java
+++ myLivingDead/src/main/java/Entity/Zombie/Type/Tank.java
@@ -4,6 +4,7 @@ import Entity.Entity;
 import Entity.Zombie.Zombie;
 
 public class Tank extends Zombie {
+
     private int health = 150;
     final private int damage = 20;
     private boolean isAlive = true;
@@ -13,9 +14,22 @@ public class Tank extends Zombie {
         typeCount = num;
     }
 
+    private int hit() {
+        // generate random number to determine if the zombie hits.
+        int max = 100;
+        int min = 1;
+        int randInt = min + (int) (Math.random() * ((max - min) + 1));
+        //These guys are heavy but can't swing their arms too well.
+        if (randInt <= 75) { //Their arms meet their target 75% of the time.
+            return damage;
+        } else { // Missed.
+            return 0;
+        }
+    }
+
     @Override
     public void doAttack(Entity target) {
-        target.onAttack(damage);
+        target.onAttack(hit());
     }
 
     @Override
